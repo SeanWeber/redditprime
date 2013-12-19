e7e2c3808b2b8d7ea0e2ed2801251d7e54a97d51
smw <bluesmew@gmail.com>
2013-12-19 02:22:04 -0500

    Thumbnails now appear on the list of articles

--- RedditReader/src/main/java/com/lightemittingsmew/redditreader/ArticleArrayAdapter.java
+++ RedditReader/src/main/java/com/lightemittingsmew/redditreader/ArticleArrayAdapter.java
@@ -1,11 +1,19 @@
 package com.lightemittingsmew.redditreader;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,19 +36,44 @@ public class ArticleArrayAdapter extends ArrayAdapter<JSONObject> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        RequestQueue queue = Volley.newRequestQueue(thisContext);
+
         LayoutInflater inflater = (LayoutInflater) thisContext
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.list_article, parent, false);
+        final View rowView = inflater.inflate(R.layout.list_article, parent, false);
+        final ImageView imageView = (ImageView) rowView.findViewById(R.id.imageViewThumbnail);
         TextView textView = (TextView) rowView.findViewById(R.id.textViewTitle);
 
         String title;
+        String thumbNailUrl = "default";
         try {
             title = articles.get(position).getString("title");
+            thumbNailUrl = articles.get(position).getString("thumbnail");
         } catch (JSONException e) {
             e.printStackTrace();
-            title = ":(";
+            title = "Could not find title";
         }
         textView.setText(title);
+
+        if(thumbNailUrl.equals("default") || thumbNailUrl.equals("")){}
+        if(thumbNailUrl.equals("self")){}
+        if(thumbNailUrl.equals("nsfw")){}
+        else{
+            try {
+                ImageRequest ir = new ImageRequest(thumbNailUrl, new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        imageView.setImageBitmap(response);
+                    }
+                }, 0, 0, null, null);
+
+                queue.add(ir);
+            }
+            catch (NullPointerException e){
+                e.printStackTrace();
+                Log.e("Problem URL", thumbNailUrl);
+            }
+        }
 
         return rowView;
     }
