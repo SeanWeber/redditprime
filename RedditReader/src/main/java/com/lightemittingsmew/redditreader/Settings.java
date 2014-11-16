dae4f60b64561e46c5dbf7fa1bd03f6e53e5b8dc
Sean Weber <bluesmew@gmail.com>
2014-11-15 22:55:19 -0500

    Fixed menu on settings and submit page

--- RedditReader/src/main/java/com/lightemittingsmew/redditreader/Settings.java
+++ RedditReader/src/main/java/com/lightemittingsmew/redditreader/Settings.java
@@ -61,22 +61,12 @@ public class Settings extends BaseActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings, menu);
-        return true;
-    }
+        super.onCreateOptionsMenu(menu);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        MenuItem settings = menu.findItem(R.id.action_settings);
+        settings.setVisible(false);
+
+        return true;
     }
 
     public void setTheme(View view){
